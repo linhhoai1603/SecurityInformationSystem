ed034b6809aa72b69a385e2ff9596f38008a54ac
HoangPer007 <22130088@st.hcmuaf.edu.vn>
2025-06-10 21:40:24 +0700

    pull request from main

---
+++ src/main/java/models/Sign.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class Sign {
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+    public static final String algorithm = "RSA";
+}
